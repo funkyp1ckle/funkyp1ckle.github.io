611e4c02eb7f1c4fc1a33fae9d1122eefda1d9a4
funkypickle <46251291+funkyp1ckle@users.noreply.github.com>
2022-04-07 19:34:39 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 var navBar = document.querySelector(".navBar");
 var expandBtn = document.getElementById("expandBtn");
 
-var navLinks = document.getElementById("navLinks").getElementsByTagName("ol")[0].getElementsByTagName("a");
-
 function initTabbedMenu() {
   var tabbedMenu = document.querySelector(".tabbed-pane");
-  var tabbedPanel = tabbedMenu.querySelector(".tabbed-pane-inner").getElementsByTagName("input");
-  var tabbedContent = tabbedMenu.querySelector(".job-container").getElementsByClassName("job");
+  var tabbedPanel = tabbedMenu
+    .querySelector(".tabbed-pane-inner")
+    .getElementsByTagName("input");
+  var tabbedContent = tabbedMenu
+    .querySelector(".job-container")
+    .getElementsByClassName("job");
 
   var lastSelectedPanelIdx = 0;
   tabbedContent[0].style.display = "block";
 
   for (let i = 0; i < tabbedPanel.length; i++) {
-    tabbedPanel[i].addEventListener("change", function() {
+    tabbedPanel[i].addEventListener("change", function () {
       tabbedContent[lastSelectedPanelIdx].style.display = "none";
       tabbedContent[i].style.display = "block";
       lastSelectedPanelIdx = i;
@@ -22,34 +24,42 @@ function initTabbedMenu() {
 
 expandBtn.addEventListener("click", function expand() {
   if (!expandBtn.checked) {
-    navBar.style.height = window.getComputedStyle(document.documentElement).getPropertyValue("--navBar-height");
+    navBar.style.height = window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue("--navBar-height");
   } else {
-    navBar.style.height = window.getComputedStyle(document.documentElement).getPropertyValue("--expanded-navBar-height");
+    navBar.style.height = window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue("--expanded-navBar-height");
   }
 });
 
-window.addEventListener("resize", function reset(event) {
+window.addEventListener("resize", function reset() {
   if (window.innerWidth > 768) {
     expandBtn.checked = false;
-    navBar.style.height = window.getComputedStyle(document.documentElement).getPropertyValue("--navBar-height");
+    navBar.style.height = window
+      .getComputedStyle(document.documentElement)
+      .getPropertyValue("--navBar-height");
   }
 });
 
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        if(window.innerWidth <= 768 && expandBtn.checked) {
-          navBar.classList.add("noTransition");
-          navBar.style.height = window.getComputedStyle(document.documentElement).getPropertyValue("--navBar-height");
-          navBar.offsetHeight;
-          navBar.classList.remove("noTransition");
-          expandBtn.checked = false;
-        }
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: "smooth",
-            block: "start"
-        });
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+    if (window.innerWidth <= 768 && expandBtn.checked) {
+      navBar.classList.add("noTransition");
+      navBar.style.height = window
+        .getComputedStyle(document.documentElement)
+        .getPropertyValue("--navBar-height");
+      navBar.offsetHeight;
+      navBar.classList.remove("noTransition");
+      expandBtn.checked = false;
+    }
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+      block: "start",
     });
+  });
 });
 
 initTabbedMenu();
