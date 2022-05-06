c784c0ec7cb29dcce9693ac06bb782930eeb945f
funkypickle <46251291+funkyp1ckle@users.noreply.github.com>
2022-05-06 19:03:04 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -21,7 +21,7 @@ function initTabbedMenu() {
   backBtn.disabled = true;
 
   nextBtn.addEventListener("click", function nextJob() {
-    if(lastSelectedPanelIdx == tabbedContent.length - 2) {
+    if(lastSelectedPanelIdx == tabbedContent.length - 1) {
       nextBtn.disabled = true;
     } else {
       tabbedContent[lastSelectedPanelIdx].style.display = "none";
